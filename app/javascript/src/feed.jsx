e3fe73eb913c336d8cf0033a8efa0c43436e0072
Nathan Peters <peters.nate8@gmail.com>
2024-05-22 18:53:11 -0600

    improved design of home page

--- app/javascript/src/feed.jsx
+++ app/javascript/src/feed.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { GetAllTweets, PostTweet, DeleteTweet } from './requests';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 class Feed extends React.Component {
   constructor(props) {
@@ -51,12 +53,12 @@ class Feed extends React.Component {
     const { currentUser, searchResults, showSearchResults, searchTerm, windowWidth } = this.props;
     const tweets = showSearchResults ? searchResults : this.state.tweetList;
     return (
-      <div className='col-12 col-md-9 mt-4 mt-md-5'>
-        <h1 className='text-center'>Twitter</h1>
+      <div className='col-12 col-md-8 col-xl-9 px-0 mb-0 pb-0 vh-100 bg-secondary'>
         <form
-          className='twitter-composer col-12 d-flex flex-column align-items-center mt-4'
+          className='twitter-composer d-flex flex-column align-items-center mx-0 pb-5 bg-light'
           onSubmit={this.handleSubmit}
         >
+          <h1 className='text-center my-4 my-md-5'>Twitter</h1>
           <div className='col-8'>
             <input
               className='form-control form-control-lg'
@@ -68,7 +70,7 @@ class Feed extends React.Component {
               onChange={this.handleMessageChange}
             />
           </div>
-          <div className='col-6 d-flex justify-content-around mt-3'>
+          <div className='col-7 col-md-6 d-flex justify-content-around mt-3'>
             <button className='btn btn-primary'>Upload Image</button>
             <button className='btn btn-primary' type='submit'>
               Tweet
@@ -76,29 +78,45 @@ class Feed extends React.Component {
           </div>
         </form>
         <div className='content col-12 d-flex flex-column align-items-center mt-5'>
-          <button className={'btn btn-warning align-self-start ' + (showSearchResults ? '' : 'd-none' )} onClick={() => {window.location.href = '/home';}}>Back</button>
-          <div className='card-deck h-50 w-50'>
+          <button
+            className={'btn btn-warning align-self-start ' + (showSearchResults ? '' : 'd-none')}
+            onClick={() => {
+              window.location.href = '/home';
+            }}
+          >
+            Back
+          </button>
+          <div className={'card-deck h-50 mt-3 ' + (windowWidth < 992 ? 'w-75' : 'w-50')}>
             {(() => {
               if (tweets.length === 0) {
                 return <h1 className='text-center'>No Search Results</h1>;
               }
-              if(searchTerm === '' && showSearchResults) {
+              if (searchTerm === '' && showSearchResults) {
                 return <h1 className='text-center'>Search for something</h1>;
               }
               return tweets.map((tweet) => (
-                <div className='tweet card border mb-5' key={tweet.id}>
-                  <div className='card-body d-flex flex-column'>
-                    <button
-                      className={'btn btn-sm border align-self-end' + (currentUser !== tweet.username ? ' d-none' : '')}
-                      onClick={() => this.handleDeleteTweet(tweet.id)}
-                    >
-                      Delete
-                    </button>
-                    <Link to={`/home/${tweet.username}`} className='h6 btn fw-light p-0 align-self-start'>
-                      @{tweet.username}
-                    </Link>
+                <div className='tweet card mb-5 border-0' key={tweet.id}>
+                  <div className='card-body d-flex flex-column bg-secondary text-light'>
+                    <div className='d-flex flex-row justify-content-between'>
+                      <Link to={`/home/${tweet.username}`} className='h5 btn fw-light text-info p-0 align-self-end'>
+                        @{tweet.username}
+                      </Link>
+                      <button
+                        className={
+                          'btn btn-outline-light btn-sm' +
+                          (currentUser !== tweet.username ? ' d-none' : '')
+                        }
+                        onClick={() => this.handleDeleteTweet(tweet.id)}
+                      >
+                        <FontAwesomeIcon icon={faTrash} />
+                      </button>
+                    </div>
                     <hr />
-                    <h5 className=''>{(showSearchResults && searchTerm !== '' ? this.highlightSearchTerm(tweet.message, searchTerm) : tweet.message)}</h5>
+                    <h5 className=''>
+                      {showSearchResults && searchTerm !== ''
+                        ? this.highlightSearchTerm(tweet.message, searchTerm)
+                        : tweet.message}
+                    </h5>
                     {/* <img src='https://picsum.photos/400' alt='placeholder' /> */}
                   </div>
                 </div>
--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { GetAllTweets, UserSignOut, PostTweet, Authenticate, DeleteTweet, GetUserTweets, SearchTweets } from './requests';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Feed from './feed';
 import Profile from './profile';
 import './home.scss';
@@ -66,91 +68,93 @@ const Home = () => {
 
   return (
     <Router>
-      <div className='row'>
-        <div
-          className={
-            'col-12 col-md-3 bg-light d-flex align-items-center justify-content-center ' +
-            (windowWidth > 768 ? 'position-sticky top-0 vh-100' : '')
-          }
-        >
-          <nav className={'navbar ' + (windowWidth < 768 ? 'navbar-expand-sm' : '')}>
-            <ul className='navbar-nav d-flex align-items-center gap-2'>
-              <li className={'nav-item ' + (windowWidth > 768 ? 'my-1' : '')}>
-                <h4 className='nav-link'>@{username}</h4>
-              </li>
-              <hr className={'w-100 mb-5 ' + (windowWidth < 768 ? 'd-none' : '') }/>
-              <li className={'nav-item ' + (windowWidth > 768 ? 'my-4' : '')}>
-                <form className='btn-group' onSubmit={handleSearchSubmit}>
-                  <input
-                    className={'form-control ' + (windowWidth > 768 ? '' : 'form-control-sm')}
-                    type='search'
-                    name='tweet-search'
-                    placeholder='Search'
-                    onChange={(event) => setSearch(event.target.value)}
-                    value={search}
-                  />
-                  <button className='btn btn-sm btn-secondary' type='submit'>
+      <div className="container-xl">
+        <div className='row'>
+          <div
+            className={
+              'col-12 col-md-4 col-xl-3 bg-light d-flex align-items-center justify-content-center border ' +
+              (windowWidth >= 768 ? 'position-sticky top-0 vh-100 border-right' : 'border-bottom')
+            }
+          >
+            <nav className={'navbar ' + (windowWidth < 768 ? 'navbar-expand-sm' : '')}>
+              <ul className='navbar-nav d-flex align-items-center gap-2'>
+                <li className={'nav-item ' + (windowWidth >= 768 ? 'my-1' : '')}>
+                  <h4 className='nav-link'>@{username}</h4>
+                </li>
+                <hr className={'w-100 mb-5 ' + (windowWidth < 768 ? 'd-none' : '')} />
+                <li className={'nav-item ' + (windowWidth >= 768 ? 'my-4' : '')}>
+                  <form className='btn-group' onSubmit={handleSearchSubmit}>
+                    <input
+                      className={'form-control ' + (windowWidth >= 768 ? '' : 'form-control-sm')}
+                      type='search'
+                      name='tweet-search'
+                      placeholder='Search'
+                      onChange={(event) => setSearch(event.target.value)}
+                      value={search}
+                    />
+                    <button className='btn btn-sm btn-secondary' type='submit'>
+                      <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                  </form>
+                </li>
+                {/* {(() => {
+                  // if (!showSearchBar) {
+                  //   return null;
+                  // }
+                  return (
+
+                  );
+                })()} */}
+                <li className={'nav-item ' + (windowWidth >= 768 ? 'my-4' : '')}>
+                  <button
+                    className={
+                      'nav-link p-4 py-3 btn ' +
+                      (windowWidth >= 768 ? ' btn-primary text-light' : 'btn-sm text-decoration-underline navbar-sm')
+                    }
+                  >
+                    Home
+                  </button>
+                </li>
+                {/* <li className={'nav-item ' + (windowWidth > 768 ? 'my-4' : '')}>
+                  <button
+                    className={
+                      'nav-link p-4 py-3 btn ' +
+                      (windowWidth > 768 ? ' btn-primary text-light' : 'btn-sm text-decoration-underline') + (showSearchBar ? ' d-none' : '')
+                    }
+                    onClick={handleShowSearchBar}
+                  >
                     Search
                   </button>
-                </form>
-              </li>
-              {/* {(() => {
-                // if (!showSearchBar) {
-                //   return null;
-                // }
-                return (
-
-                );
-              })()} */}
-              <li className={'nav-item ' + (windowWidth > 768 ? 'my-4' : '')}>
-                <button
-                  className={
-                    'nav-link p-4 py-3 btn ' +
-                    (windowWidth > 768 ? ' btn-primary text-light' : 'btn-sm text-decoration-underline navbar-sm')
-                  }
-                >
-                  Home
-                </button>
-              </li>
-              {/* <li className={'nav-item ' + (windowWidth > 768 ? 'my-4' : '')}>
-                <button
-                  className={
-                    'nav-link p-4 py-3 btn ' +
-                    (windowWidth > 768 ? ' btn-primary text-light' : 'btn-sm text-decoration-underline') + (showSearchBar ? ' d-none' : '')
-                  }
-                  onClick={handleShowSearchBar}
-                >
-                  Search
-                </button>
-              </li> */}
-              <li className={'nav-item ' + (windowWidth > 768 ? 'my-2' : '')}>
-                <button
-                  className={
-                    'nav-link p-4 py-3 btn ' +
-                    (windowWidth > 768 ? ' btn-primary text-light' : 'btn-sm text-decoration-underline navbar-sm')
-                  }
-                  onClick={handleLogOut}
-                >
-                  Log Out
-                </button>
-              </li>
-            </ul>
-          </nav>
+                </li> */}
+                <li className={'nav-item ' + (windowWidth >= 768 ? 'my-2' : '')}>
+                  <button
+                    className={
+                      'nav-link p-4 py-3 btn ' +
+                      (windowWidth >= 768 ? ' btn-primary text-light' : 'btn-sm text-decoration-underline navbar-sm')
+                    }
+                    onClick={handleLogOut}
+                  >
+                    Log Out
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <Switch>
+            <Route path='/home' exact>
+              <Feed
+                currentUser={username}
+                searchResults={searchResults}
+                showSearchResults={showSearchResults}
+                searchTerm={search}
+                windowWidth={windowWidth}
+              />
+            </Route>
+            <Route path='/home/*'>
+              <Profile currentUser={username} />
+            </Route>
+          </Switch>
         </div>
-        <Switch>
-          <Route path='/home' exact>
-            <Feed
-              currentUser={username}
-              searchResults={searchResults}
-              showSearchResults={showSearchResults}
-              searchTerm={search}
-              windowWidth={windowWidth}
-            />
-          </Route>
-          <Route path='/home/*'>
-            <Profile currentUser={username} />
-          </Route>
-        </Switch>
       </div>
     </Router>
   );
